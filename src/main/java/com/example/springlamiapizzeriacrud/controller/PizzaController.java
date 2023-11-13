befe12746786d29fd6915019f27f0e9f0adef634
DanieleCorrado <corrado_daniele@virgilio.it>
2023-11-13 14:24:33 +0100

    add first part

--- src/main/java/com/example/springlamiapizzeriacrud/controller/PizzaController.java
+++ src/main/java/com/example/springlamiapizzeriacrud/controller/PizzaController.java
@@ -3,12 +3,17 @@ package com.example.springlamiapizzeriacrud.controller;
 import com.example.springlamiapizzeriacrud.model.Pizza;
 import com.example.springlamiapizzeriacrud.repository.PizzaRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
+import java.util.Optional;
 
 @Controller
 @RequestMapping("/pizzas")
@@ -20,11 +25,34 @@ public class PizzaController {
     private PizzaRepository pizzaRepository;
 
     @GetMapping
-    public String index(Model model) {
+    public String index(@RequestParam Optional<String> search, Model model) {
 
-        List<Pizza> pizzaList = pizzaRepository.findAll();
-        model.addAttribute("pizzaList", pizzaList);
+        List<Pizza> pizzaList;
 
-        return "pizzas/list";
+        // Se il parametro di ricerca è presente filtro la lista dei libri
+
+        if (search.isPresent()) {
+            pizzaList = pizzaRepository.findByNameContainingIgnoreCase(search.get());
+        } else  {
+
+            // bookRepository recupera da database la lista di tutti i libri
+
+            pizzaList = pizzaRepository.findAll();
+        }
+
+            model.addAttribute("pizzaList", pizzaList);
+
+            return "pizzas/list";
+    }
+
+    @GetMapping("/show/{id}")
+    public String show(@PathVariable Integer id, Model model) {
+
+
+        Pizza pizza = pizzaRepository.findById(id).orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Pizza with id " + id + " not found"));
+        model.addAttribute("pizza",pizza);
+
+        return "pizzas/show";
+
     }
 }
